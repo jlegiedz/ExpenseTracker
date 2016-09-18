6adeb8ecf9ba1562a74d1d5902397f204a9a711c
Joanna Legiędź <Joanna Legid>
2016-09-18 22:01:44 +0200

    first commit

---
+++ ExpenseTracker/Controllers/ExpensesController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private ExpenseDBContext db = new ExpenseDBContext();
+
+        // GET: Expenses
+        public ActionResult Index()
+        {
+            return View(db.Expenses.ToList());
+        }
+
+        // GET: Expenses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expense);
+        }
+
+        // GET: Expenses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Expenses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Amount,Category,Date")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Add(expense);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(expense);
+        }
+
+        // GET: Expenses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expense);
+        }
+
+        // POST: Expenses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Amount,Category,Date")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(expense).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+        }
+
+        // GET: Expenses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expense);
+        }
+
+        // POST: Expenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Expense expense = db.Expenses.Find(id);
+            db.Expenses.Remove(expense);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public Category Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public enum Category
+    {
+        Food,
+        Car,
+        Children,
+        Health,
+        Beauty,
+        House,
+        Cloths,
+        Entertiment
+
+    }
+
+    public class ExpenseDBContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
