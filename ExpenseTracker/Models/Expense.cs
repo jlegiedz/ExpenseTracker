3b42081cfb33910bfd14379944e7d250f7320972
Joanna Legiędź <Joanna Legid>
2016-10-03 09:31:10 +0200

    Validation.

--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -7,9 +7,15 @@ namespace ExpenseTracker.Models
     public class Expense
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter the expense amount.")]
         public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Please choose a category from the list.")]
         public Category Category { get; set; }
+
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
     }
 
